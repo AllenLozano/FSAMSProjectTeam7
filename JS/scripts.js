2f46f72021eb5bac9f224ec350415e0047698d9d
AllenLozano <43081856+AllenLozano@users.noreply.github.com>
2019-09-26 18:55:21 -0700

    Update scripts.js

--- JS/scripts.js
+++ JS/scripts.js
@@ -21,3 +21,10 @@ function stopClock() {
   clearInterval(clockID);
   clockOff =true;
 }:
+
+/* Blue print will be split into 7 different grids, one of the 7 grids will be a long one consisting in the same length of
+the other two that has 3 grids, to complete one big blueprint grid. Each division will have different 
+ammounts of sprinklers and alarm systems in each room/grid. Every room has a door, and we will have tw0 exit doors, one north
+and one South. We will simulate that the fire is growing. We will have a random(1-7) method that will pick one of he 7 grids
+and it will catch on fire, randomly. We will have grids say on or off for the sprinkler. Instead of putting out the fire
+which might be. "Random(fire) - If(hasSprinker(), sprinkler = 1(on))."
